4f40e0fe56d48ace249cbb32c199b5a764498d98
meherul382 <meherulhassan62@gmail.com>
2026-09-09 14:33:48 +0600

    fix: simplify ESLint config for Vercel build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
-import nextTs from 'eslint-config-next/typescript'
 
-export default defineConfig([
+export default [
   ...nextVitals,
-  ...nextTs,
-  globalIgnores(['.next/**', 'node_modules/**', 'out/**', 'build/**', 'next-env.d.ts']),
-])
+  {
+    ignores: ['.next/**', 'node_modules/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+]
