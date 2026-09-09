1a4e825bf763d376ff6117465dfe06c8c9dc6ee6
meherul382 <meherulhassan62@gmail.com>
2026-09-09 14:39:59 +0600

    fix: remove incompatible ESLint config import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,4 @@
-import nextVitals from 'eslint-config-next/core-web-vitals.js'
-
 export default [
-  ...nextVitals,
   {
     ignores: ['.next/**', 'node_modules/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
